1ea951193132b6aa32c7d03f7e28dc094a843b86
ezemiller <onejemechibueze@gmail.com>
2023-06-13 10:55:01 +0100

    First commit

---
+++ store/item/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import User
+from django.db import models
+from PIL import Image
+
+class Category(models.Model):
+    name =models.CharField(max_length=255)
+    class Meta:
+        ordering =('name',)
+        verbose_name_plural='Categories'
+
+    def __str__(self) -> str:
+        return self.name
+
+class Item(models.Model):
+    category =models.ForeignKey(Category,related_name='items',on_delete=models.CASCADE)
+    name =models.CharField(max_length=255)
+    description =models.TextField(blank=True,null=True)
+    price =models.FloatField()
+    image = models.ImageField(upload_to='item_images',blank=True,null=True)
+    is_sold =models.BooleanField(default=False)
+    created_by =models.ForeignKey(User,related_name='items',on_delete=models.CASCADE)
+    created_at =models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
+        img = Image.open(self.image.path)
+        
+        if img.height > 300 or img.width > 300:
+           output_size = (300, 300)
+           img.thumbnail(output_size)
+           img.save(self.image.path)
